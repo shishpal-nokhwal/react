73940eaeff9ba90fddde8ff354958c25243fda08
shishpal-nokhwal <nokhwalshishpal29@gmail.com>
2024-02-09 13:50:47 +0530

    section update

---
+++ basic/src/componets/footer.js
@@ -0,0 +1,12 @@
+import "./footer.css"
+function Footer(){
+    return(
+<div>
+    <p className="ab">Quick Start</p>
+    <p className="aa">Welcome to the React documentation! This page will
+     give you an introduction to the 80% of React concepts that you will use on a daily basis.</p>
+</div>
+    )
+}
+
+export default Footer
---
+++ basic/src/componets/header.js
@@ -0,0 +1,19 @@
+// function Header(){
+//     return(
+//         <div>
+//             <p>hello</p>
+//         </div>
+//     )
+// }
+// export default Header
+import { Component } from "react";
+
+
+class Header extends Component{
+    render(){
+        return(
+            <h1 className="s">shishpal</h1>
+        )
+    }
+}
+export default Header
